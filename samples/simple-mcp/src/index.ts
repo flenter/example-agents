5bb4f98fc223a1f36c0281f6f1f4d761149f6c80
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2025-05-22 12:22:10 +0200

    add scaffold for mcp server

---
+++ samples/simple-mcp/src/index.ts
@@ -0,0 +1,22 @@
+import { Hono } from "hono";
+import { McpAgent } from "agents/mcp";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+
+
+// MCP Server
+
+export class BlueskyContextServer extends McpAgent {
+	server = new McpServer({ name: "Bluesky Context Server", version: "1.0.0" });
+	async init() {}
+}
+
+
+// entrypoint
+
+const app = new Hono();
+
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
+
+export default app;
